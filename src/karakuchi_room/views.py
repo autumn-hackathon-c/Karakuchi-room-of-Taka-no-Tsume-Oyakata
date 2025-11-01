29da33ba742a8eb2259929a37e9886c10a9ace3a
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-11-01 17:02:37 +0900

    fix:コメントアウト追記

--- src/karakuchi_room/views.py
+++ src/karakuchi_room/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
+# from .forms import SurveyForm
 
 from karakuchi_room.models import Survey
 
@@ -21,6 +22,7 @@ class SurveyCreateView(CreateView):
     model = Survey
     fields = "__all__"
     template_name = "karakuchi_room/surveys_create.html"
+    # form_class = SurveyForm
     #成功後、詳細画面にリダイレクト
     success_url = reverse_lazy("surveys-detail")
 
