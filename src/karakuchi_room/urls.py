8e11d2042749fa1478e846f9f5c157c471ac1af2
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-11-01 16:29:01 +0900

    fix:アンケート詳細画面のロジック作成

--- src/karakuchi_room/urls.py
+++ src/karakuchi_room/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from karakuchi_room.views import SurveyListView, SurveyDetailView, SurveyCreateView, SurveyDeleteView, SurveyUpdateView
 
 urlpatterns = [
+    # アンケート一覧
     path("surveys/", SurveyListView.as_view(), name="survey-list"),
-    path("surveys/create/", SurveyCreateView.as_view(), name="survey-create")
+    
+    # アンケート新規作成
+    path("surveys/create/", SurveyCreateView.as_view(), name="survey-create"),
+    
+    # アンケート詳細
+    path("surveys/detail/<int:pk>", SurveyDetailView.as_view(), name="survey-detail"),
 
 ]
