a1488718869711df6f3e89c7823f483f8c97f87f
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-11-07 01:26:47 +0900

    トイトイ:リファクタリング

--- src/sample/settings.py
+++ src/sample/settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # 未ログイン状態でログインが必要なページにアクセスしたときに
 # ログインページにリダイレクトされる
 LOGIN_URL = "login"
-# ログインしたらアンケート詳細画面にリダイレクト
-LOGIN_REDIRECT_URL = 'surveys'
+# ログインしたらアンケート一覧画面にリダイレクト
+LOGIN_REDIRECT_URL = 'survey-list'
 # ログアウトしたらログインページにリダイレクト
 LOGOUT_REDIRECT_URL = 'login'
 
