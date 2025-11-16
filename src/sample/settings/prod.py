d46d02ec9948758a096637e377bfd83693bd3897
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2025-11-16 17:04:22 +0900

    コメントアウト再度記載

--- src/sample/settings/prod.py
+++ src/sample/settings/prod.py
@@ -19,6 +19,7 @@ DATABASES = {
         "PASSWORD": os.environ["DB_PASSWORD"],
         "HOST": os.environ["DB_HOST"],
         "PORT": os.getenv("DB_PORT", "3306"),
+        # 絵文字の文字化け対策
         "OPTIONS": {
             "charset": "utf8mb4",
             "use_unicode": True,
@@ -31,20 +32,30 @@ DATABASES = {
 #  S3 (IAMロールを使う想定)
 # =========================
 
-# ← ここでは AWS_ACCESS_KEY_ID / SECRET は **使わない**
-#    コンテナは EC2 の IAM ロール経由で S3 にアクセスさせる
+# AWS_ACCESS_KEY_ID / SECRET は **使わない**
+# コンテナは EC2 の IAM ロール経由で S3 にアクセスさせる
 
+# パケット名
 AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+
+# リージョン名
 AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "ap-northeast-1")
 
+# S3 のカスタムドメイン（静的ファイルの配信用 URL）
 AWS_S3_CUSTOM_DOMAIN = os.getenv(
     "AWS_S3_CUSTOM_DOMAIN",
     f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com",
 )
 
+# 署名・公開URLの推奨
 AWS_S3_SIGNATURE_VERSION = "s3v4"
+
 AWS_DEFAULT_ACL = None
+
+# 署名付きクエリ
 AWS_QUERYSTRING_AUTH = True
+
+# 同名アップ時の上書き防止（任意）
 AWS_S3_FILE_OVERWRITE = False
 
 AWS_S3_OBJECT_PARAMETERS = {
@@ -55,6 +66,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = "static"  # S3 上のフォルダ名
 
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+
+# collectstatic の送り先を S3 に変更
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 
 
@@ -62,10 +75,12 @@ STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+# CSRF信頼オリジン
 CSRF_TRUSTED_ORIGINS = [
     "https://karakuchi-room.com",
     "https://www.karakuchi-room.com",
 ]
 
+# セキュアCookie
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
